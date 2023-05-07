12f1a112e3a7cdd416690d64b0625e534a3963d4
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:39:54 +0300

    Create ContactForm.styled.js

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+display: block;
+margin-top: 20px;
+margin-bottom: 20px;
+`;
+
+export const FormInput = styled.input`
+padding: 10px;
+background-color: white;
+border: 1px solid #d8e4e2;
+border-radius: 8px;
+`;
+
+export const SubmitButton = styled.button`
+padding: 8px 15px;
+margin: 10px;
+font-size: 12px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+color: #fff;
+background-color: #4CAF50;
+border: none;
+border-radius: 15px;
+`;
+
+
+export const FormLabel = styled.label`
+  margin-right: 15px;
+`;
+
+
