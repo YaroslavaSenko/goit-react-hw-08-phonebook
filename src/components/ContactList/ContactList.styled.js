f9b6abbf9f111e78704b57d1f6ed89ae22d5ead1
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:40:04 +0300

    Create ContactList.styled.js

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+
+export const Button = styled.button`
+padding: 8px 15px;
+margin: 10px;
+font-size: 12px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+color: #fff;
+background-color: #4CAF50;
+border: none;
+border-radius: 20px;
+`;
