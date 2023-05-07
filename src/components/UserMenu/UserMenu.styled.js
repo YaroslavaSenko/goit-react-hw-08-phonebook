d715c935c57d252daee03cac651c3472f468c5df
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:41:09 +0300

    Create UserMenu.styled.js

---
+++ src/components/UserMenu/UserMenu.styled.js
@@ -0,0 +1,11 @@
+
+import styled from '@emotion/styled';
+
+
+export const Wrapper = styled.wrapper`
+display: flex;
+    align-items: center;
+    gap: 12px;`
+
+export const Username = styled.p`
+font-weight: 700`
