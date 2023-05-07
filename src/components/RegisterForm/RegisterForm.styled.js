49fd01aca02885227319548d52be761685ef534f
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:41:00 +0300

    Create RegisterForm.styled.js

---
+++ src/components/RegisterForm/RegisterForm.styled.js
@@ -0,0 +1,12 @@
+
+import styled from '@emotion/styled';
+
+
+export const Form = styled.form`
+width: 320px;`
+
+export const Label = styled.label`
+display: flex;
+display: flex;
+    flex-direction: column;
+    margin-bottom: 16px;`
