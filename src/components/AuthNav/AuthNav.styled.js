0cb9aa2d628925509edff90bff6fd8370f04f3de
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:39:43 +0300

    Update AuthNav.styled.js

--- src/components/AuthNav/AuthNav.styled.js
+++ src/components/AuthNav/AuthNav.styled.js
@@ -1,13 +1,25 @@
-import styled from "styled-components";
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 
+export const AuthNavArea = styled.nav``;
 
-export const Link = styled.link`
- display: inline-block;
-    text-decoration: none;
-    padding: 12px;
-    font-weight: 700;
-    color: #2a363b;
+export const AuthNavUl = styled.ul`
+  display: flex;
+  li:not(:last-child) {
+    margin-right: 20px;
+  }
+`;
 
-.active {
-    color: #e84a5f;
-  }`
+export const AuthNavLI = styled.li`
+  text-decoration: none;
+`;
+
+export const AuthNaviLink = styled(NavLink)`
+  text-decoration: none;
+  :visited {
+    color: inherit;
+  }
+  :hover {
+    color: green;
+  }
+`;
