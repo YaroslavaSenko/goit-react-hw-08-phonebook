8e5a8f5e4836f9dbe65ef8613aacfb0122a5a19e
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:39:31 +0300

    Update AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -9,3 +9,9 @@ export const Header = styled.header`
   padding: 8px 0;
   margin-bottom: 16px;
   border-bottom: 1px solid black;`
+
+export const AuthArea = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+`;
