478e03bfd98098b0a3d24ac7a47be1c80fa294da
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-07 16:40:24 +0300

    Create ContactsListItem.styled.js

---
+++ src/components/ContactsListItem/ContactsListItem.styled.js
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Contact = styled.li`
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+`;
+
+export const DeleteBtn = styled.button`
+ padding: 8px 15px;
+margin: 10px;
+font-size: 12px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+color: #fff;
+background-color: #4CAF50;
+border: none;
+border-radius: 15px;
+`;
+
