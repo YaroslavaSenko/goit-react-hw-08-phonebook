281ef4c66934964fed8be0f676e39c7074db22a5
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:43:44 +0300

    Create Filter.styled.jsx

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const FilterLabel = styled.label`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+padding: 10px;
+background-color: white;
+border: 1px solid #d8e4e2;
+border-radius: 8px;
+margin-left: 20px;
+`;
