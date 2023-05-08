a23787dd3c5bd222eb7c26cc0b3c78aa3e404df3
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:50:47 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,72 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
-// import { nanoid } from 'nanoid';
-import { Form,
-  FormLabel,
-  FormInput,
-  SubmitButton} from './ContactForm.styled';
+import React from 'react';
+import { useAddContactMutation } from 'redux/contacts/contactsSlice';
+import { Form } from './Form/Form';
 
-import { addContacts,  } from 'redux/contacts/operations';
-import { getContacts } from 'redux/contacts/selectors';
+export const ContactForm = ({ data }) => {
+  const [addContact] = useAddContactMutation();
 
+  const formSubmitHandler = contactToAdd => {
+    const nameMatches = data.find(
+      contact => contact.name.toLowerCase() === contactToAdd.name.toLowerCase()
+    );
 
-export default function ContactForm() {
-  const contacts = useSelector(getContacts);
-
-  const dispatch = useDispatch();
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    const form = event.target;
-    const name = form.elements.name.value;
-    const number = form.elements.number.value;
-
-    // const contact = {
-    //   id: nanoid(),
-    //   name,
-    //   number,
-    // };
-    if (
-      contacts.find(
-        contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
-      )
-    ) {
-      return alert(`${name} is already in contacts.`);
-    } else{
-      dispatch(addContacts({name, number}));
+    if (nameMatches) {
+      return alert(`${contactToAdd.name} is already in contacts.`);
     }
-    event.target.reset();
+
+    addContact(contactToAdd);
+    
   };
 
-
-  return (
-    <Form onSubmit={handleSubmit}>
-    <h1>Phonebook</h1> 
-
-      <FormLabel>
-        <h3>Name</h3>
-      
-        <FormInput
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </FormLabel>
-      <FormLabel>
-        <h3>  Number</h3>      
-    
-        <FormInput
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-      </FormLabel>
-      <br />
-
-      <SubmitButton type="submit">Add contact</SubmitButton>
-    </Form>
-  );
-}
+  return <Form onSubmit={formSubmitHandler} btnText="Add contact" />;
+};
