b2b5dab0944dfc08ec0f91adbcbd46347691f76d
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:45:30 +0300

    Create UserMenu.styled.jsx

---
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  text-align: center;
+`;
+export const Button = styled.button`
+padding: 8px 15px;
+margin: 10px;
+font-size: 12px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+color: #fff;
+background-color: #4CAF50;
+border: none;
+border-radius: 15px;
+`;
