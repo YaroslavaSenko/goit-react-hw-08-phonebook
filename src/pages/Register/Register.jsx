4f70b4725f9f85b31d1c85fb806d9c156945a870
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:47:59 +0300

    Create Register.jsx

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+import { RegisterContainer } from './Register.styled';
+
+const Register = () => {
+  return (
+    <RegisterContainer>
+      <RegisterForm />
+    </RegisterContainer>
+  );
+};
+
+export default Register;
