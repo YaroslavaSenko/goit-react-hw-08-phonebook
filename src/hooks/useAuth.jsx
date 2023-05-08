2d2a205d7fee50a8895ef63afd48b1334c16da59
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:45:44 +0300

    Update useAuth.jsx

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,18 +1,21 @@
 import { useSelector } from 'react-redux';
 import {
-  selectUser,
+  selectError,
   selectIsLoggedIn,
   selectIsRefreshing,
-} from 'redux/auth/selectors';
+  selectUser,
+} from 'redux/auth/authSelectors';
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
+  const error = useSelector(selectError);
 
   return {
     isLoggedIn,
     isRefreshing,
     user,
+    error,
   };
 };
