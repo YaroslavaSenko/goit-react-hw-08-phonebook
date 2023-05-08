dfae2a2764ec1a3fd680cc926c6443fda0bec726
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:42:47 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,8 @@
-import { Navigation } from '../Navigation/Navigation';
-import { UserMenu } from '../UserMenu/UserMenu';
-import { AuthNav } from '../AuthNav/AuthNav';
-import { useAuth } from 'hooks';
-import {Header,AuthArea } from './AppBar.styled';
-
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { Navigation } from 'components/Navigation/Navigation';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+import { useAuth } from 'hooks/useAuth';
+import { AuthArea, Header } from './AppBar.styled';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
