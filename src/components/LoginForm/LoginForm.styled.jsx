a20fe408c9f6bc96e0fe49c80c4a0ba844b4a247
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:44:25 +0300

    Create LoginForm.styled.jsx

---
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+display: flex;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const FormLabel = styled.label`
+ margin-right: 15px;
+`;
+
+export const FormInput = styled.input`
+padding: 10px;
+background-color: white;
+border: 1px solid #d8e4e2;
+border-radius: 8px;
+margin-top: 10px;
+`;
+
+
+
+export const Button = styled.button`
+padding: 8px 15px;
+margin: 10px;
+font-size: 12px;
+cursor: pointer;
+text-align: center;
+text-decoration: none;
+color: #fff;
+background-color: #4CAF50;
+border: none;
+border-radius: 15px;
+`;
+
+
+
+
+
