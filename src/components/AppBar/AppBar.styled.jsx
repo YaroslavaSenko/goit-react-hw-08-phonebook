ba8fc4dd8f83a8fe4a3e060c995dcf6ecc399906
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-05-08 09:42:58 +0300

    Create AppBar.styled.jsx

---
+++ src/components/AppBar/AppBar.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const AuthArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
